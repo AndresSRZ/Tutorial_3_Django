0724c244c08d564f5c76cae616944c22d1e1f102
Andres <asuarezr2@eafit.edu.co>
2025-02-18 15:36:19 -0500

    Commit_2

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductSuccessView, ContactPageView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ProductSuccessView, ContactPageView, CartView, CartRemoveAllView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
@@ -7,6 +7,13 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/success/', ProductSuccessView.as_view(), name='success'), 
+    
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
+
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+   
     path('contact/', ContactPageView.as_view(), name='contact'), 
 ]
